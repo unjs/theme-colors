87522f0bbd11dde80025beff88ce4fd9a58a553a
Johann Schopplich <mail@johannschopplich.com>
2023-10-28 14:27:49 +0200

    chore: update repository (#30)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { withTint, withShade, parseColor, hexValue } from './utils'
+import { withTint, withShade, parseColor, hexValue } from "./utils";
 
 export const _variants = {
   50: withTint(0.95),
@@ -6,18 +6,20 @@ export const _variants = {
   200: withTint(0.75),
   300: withTint(0.6),
   400: withTint(0.3),
-  500: c => c,
+  500: (c: number[]) => c,
   600: withShade(0.9),
   700: withShade(0.6),
   800: withShade(0.45),
-  900: withShade(0.3)
-}
+  900: withShade(0.3),
+};
 
-export function getColors (color, variants = _variants) {
-  const colors = {}
-  const components = parseColor(color)
+export function getColors(color: string, variants = _variants) {
+  const colors: Record<string, string> = {};
+  const components = parseColor(color);
+
   for (const [name, fn] of Object.entries(variants)) {
-    colors[name] = hexValue(fn(components))
+    colors[name] = hexValue(fn(components));
   }
-  return colors
+
+  return colors;
 }
