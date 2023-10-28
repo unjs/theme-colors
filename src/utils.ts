c230c163b480d45c1ec982c5ea6f8484ea5166b7
Johann Schopplich <mail@johannschopplich.com>
2023-10-28 14:01:18 +0200

    chore: migrate to latest UnJS template

--- src/utils.ts
+++ src/utils.ts
@@ -1,37 +1,42 @@
-export function parseColor (color = '') {
-  if (typeof color !== 'string') {
-    throw new TypeError('Color should be string!')
+export function parseColor(color = "") {
+  if (typeof color !== "string") {
+    throw new TypeError("Color should be string!");
   }
 
-  const hexMatch = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color)
+  const hexMatch = /^#?([\da-f]{2})([\da-f]{2})([\da-f]{2})$/i.exec(color);
   if (hexMatch) {
-    return hexMatch.splice(1).map(c => parseInt(c, 16))
+    return hexMatch.splice(1).map((c) => Number.parseInt(c, 16));
   }
 
-  const hexMatchShort = /^#?([a-f\d])([a-f\d])([a-f\d])$/i.exec(color)
+  const hexMatchShort = /^#?([\da-f])([\da-f])([\da-f])$/i.exec(color);
   if (hexMatchShort) {
-    return hexMatchShort.splice(1).map(c => parseInt(c + c, 16))
+    return hexMatchShort.splice(1).map((c) => Number.parseInt(c + c, 16));
   }
 
-  if (color.includes(',')) {
-    return color.split(',').map(p => parseInt(p))
+  if (color.includes(",")) {
+    return color.split(",").map((p) => Number.parseInt(p));
   }
 
-  throw new Error('Invalid color format! Use #ABC or #AABBCC or r,g,b')
+  throw new Error("Invalid color format! Use #ABC or #AABBCC or r,g,b");
 }
 
-export function hexValue (components: number[]) {
-  return '#' + components.map(c => `0${c.toString(16).toUpperCase()}`.slice(-2)).join('')
+export function hexValue(components: number[]) {
+  return (
+    "#" +
+    components.map((c) => `0${c.toString(16).toUpperCase()}`.slice(-2)).join("")
+  );
 }
 
-export function tint (components: number[], intensity: number) {
-  return components.map(c => Math.round(c + (255 - c) * intensity))
+export function tint(components: number[], intensity: number) {
+  return components.map((c) => Math.round(c + (255 - c) * intensity));
 }
 
-export function shade (components: number[], intensity: number) {
-  return components.map(c => Math.round(c * intensity))
+export function shade(components: number[], intensity: number) {
+  return components.map((c) => Math.round(c * intensity));
 }
 
-export const withTint = (intensity: number) => (hex: number[]) => tint(hex, intensity)
+export const withTint = (intensity: number) => (hex: number[]) =>
+  tint(hex, intensity);
 
-export const withShade = (intensity: number) => (hex: number[]) => shade(hex, intensity)
+export const withShade = (intensity: number) => (hex: number[]) =>
+  shade(hex, intensity);
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,43 +1,43 @@
-import { describe, it, expect } from 'vitest'
-import { getColors } from '../src/index'
-import { parseColor } from '../src/utils'
+import { describe, it, expect } from "vitest";
+import { getColors } from "../src/index";
+import { parseColor } from "../src/utils";
 
-describe('theme-colors', () => {
+describe("theme-colors", () => {
   const fixture = {
-    hex: '#ABABAB',
-    rgb: ' 171, 171,171 ',
+    hex: "#ABABAB",
+    rgb: " 171, 171,171 ",
     theme: {
-      50: '#FBFBFB',
-      100: '#F7F7F7',
-      200: '#EAEAEA',
-      300: '#DDDDDD',
-      400: '#C4C4C4',
-      500: '#ABABAB',
-      600: '#9A9A9A',
-      700: '#676767',
-      800: '#4D4D4D',
-      900: '#333333'
-    }
-  }
+      50: "#FBFBFB",
+      100: "#F7F7F7",
+      200: "#EAEAEA",
+      300: "#DDDDDD",
+      400: "#C4C4C4",
+      500: "#ABABAB",
+      600: "#9A9A9A",
+      700: "#676767",
+      800: "#4D4D4D",
+      900: "#333333",
+    },
+  };
 
-  it('getColors (hex)', () => {
-    expect(getColors(fixture.hex)).toMatchObject(fixture.theme)
-  })
+  it("getColors (hex)", () => {
+    expect(getColors(fixture.hex)).toMatchObject(fixture.theme);
+  });
 
-  it('getColors (rgb)', () => {
-    expect(getColors(fixture.rgb)).toMatchObject(fixture.theme)
-  })
+  it("getColors (rgb)", () => {
+    expect(getColors(fixture.rgb)).toMatchObject(fixture.theme);
+  });
 
-  it('getColors (invalid)', () => {
-    expect(() => getColors('red')).toThrowError(/Invalid color format!/)
-  })
+  it("getColors (invalid)", () => {
+    expect(() => getColors("red")).toThrowError(/Invalid color format!/);
+  });
 
-  it('getColors (invalid obj)', () => {
+  it("getColors (invalid obj)", () => {
     // @ts-expect-error: test invalid argument
-    expect(() => getColors({})).toThrowError(/Color should be string!/)
-  })
+    expect(() => getColors({})).toThrowError(/Color should be string!/);
+  });
 
-  it('parseColor (shorthand)', () => {
-    expect(parseColor('#09C')).toEqual(parseColor('#0099cc'))
-  })
-})
+  it("parseColor (shorthand)", () => {
+    expect(parseColor("#09C")).toEqual(parseColor("#0099cc"));
+  });
+});
